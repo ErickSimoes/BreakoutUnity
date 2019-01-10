f689862edd91a9525d3367308e2e34028fe8502d
Erick Simões <erick.simoes@live.com>
2019-01-10 16:58:04 -0300

    Bar setup and behaviour

---
+++ Assets/Scripts/BarBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarBehaviour : MonoBehaviour {
+
+    public float speed = 20f;
+
+    void Start() {
+        
+    }
+
+    void FixedUpdate() {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * Input.GetAxisRaw("Horizontal") * speed;
+    }
+}
