639e4134332c2b61a1acd4642ac0e02adedda710
Erick Simões <erick.simoes@live.com>
2019-01-11 18:13:20 -0300

    Add ground on Sample Scene
    Basic dynamics of end of game.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    /// <summary>
+    /// Finish the game
+    /// </summary>
+    public static void GameOver() {
+        Debug.Log("Game Over");
+    }
+
+}
---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour
+{
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        GameController.GameOver();
+    }
+}
