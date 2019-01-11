c46d91fe81058a58b5d88a555dd890f6a375f0f2
Erick Simões <erick.simoes@live.com>
2019-01-11 09:55:13 -0300

    Ball moviment and collision
    Moviment based on position and collision recognition.

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour {
 
+    public float speedX = 0.08f;
+    public float speedY = 0.08f;
+
     void Start() {
         
     }
 
-    void Update() {
-        
+    void FixedUpdate() {
+        transform.position = new Vector3(transform.position.x + speedX, transform.position.y + speedY);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        if (collision.gameObject.tag == "VerticalLimits") {
+            speedX = -speedX;
+        }
+
+        if (collision.gameObject.tag == "HorizontalLimits") {
+            speedY = -speedY;
+        }
+
     }
 }
