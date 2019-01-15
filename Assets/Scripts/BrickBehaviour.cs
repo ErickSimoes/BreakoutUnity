a925d5c228485121a8d9249c6544cd225cd5b795
Erick Simões <erick.simoes@live.com>
2019-01-15 10:33:58 -0300

    Count points

--- Assets/Scripts/BrickBehaviour.cs
+++ Assets/Scripts/BrickBehaviour.cs
@@ -16,6 +16,8 @@ public class BrickBehaviour : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision) {
         source.Play();
+        GameController.Points--;
+        Debug.Log("Points: " + GameController.Points);
         Destroy(gameObject, 0.044f);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,28 +5,45 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
     public Transform brick;
-    
+
+    private static int points;
+
+    public static int Points {
+        get => points;
+        set {
+            if (points <= 0) {
+                GameOver(true);
+            }
+            points = value;
+        }
+    }
+
     void Start() {
         CreateBricks();
     }
-
-    void Update() {
-        
-    }
-
+    
     /// <summary>
     /// Finish the game
+    /// True if win or false if lose.
     /// </summary>
-    public static void GameOver() {
-        Debug.Log("Game Over");
+    public static void GameOver(bool win) {
+        if (win) {
+            Debug.Log("Win!");
+        } else {
+            Debug.Log("Game Over");
+        }
     }
 
     public void CreateBricks() {
         for (float y = 2; y > 0; y = y - 1.179f) {
             for (float x = -9; x < 9; x = x + 1.196f) {
                 Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity);
+                points++;
             }
         }
     }
+
+
+
 
 }
