6ca59dfa3bd45ae21dc4c6649f8ea43a56bfb04f
Erick Simões <erick.simoes@live.com>
2019-01-16 14:15:14 -0300

    Play explosion audio when ball go to ground

--- Assets/Scripts/GroundController.cs
+++ Assets/Scripts/GroundController.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GroundController : MonoBehaviour {
-    
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision) {
+        audioSource.Play();
         GameController.state = GameController.GameOverState.lose;
     }
 }
